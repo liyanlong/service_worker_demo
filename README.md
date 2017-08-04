5b556e8022479f07e9640feafb8308cf9d40b262
liyl <295697141@qq.com>
2017-08-04 12:32:19 +0800

    update demo

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Service Worker Demo
 A simple service worker demo, it's help us to understand service worker mechanism
 
-[Online Demo](https://liyanlong.github.io/service_worker_demo/)
+[Live Demo](https://liyanlong.github.io/service_worker_demo/)
+
+or you can scan QR code
+
+![QR Code](./images/QR.png)
 
 # Usage
 
 ```javascript
   git https://github.com/liyanlong/service_worker_demo/
   npm install
-  npm run start
+  npm run dev or npm run start
 ```
 
 # License 
 [MIT](https://github.com/liyanlong/service_worker_demo/blob/master/LICENSE)
+
+
+# Relationship
+- [sw-test](https://github.com/mdn/sw-test)
+- [使用 Service Workers](https://developer.mozilla.org/zh-CN/docs/Web/API/Service_Worker_API/Using_Service_Workers)
+- [页面守护者：Service Worker](http://imweb.io/topic/56592b8a823633e31839fc01)
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
-var CACHE_VERSION = 'v1';
+var CACHE_VERSION = '20170804v1';
 
+// 存储到 浏览器的 cache storage
 var CACHE_FILE_LIST = [
   './',
   './index.html',
@@ -7,11 +8,12 @@ var CACHE_FILE_LIST = [
   './vendor/app.js',
   './vendor/vue.js',
   './vendor/welcome.jpg',
-  './vendor/404.jpg',
-  './api/default.json'  
+  './vendor/404.jpg'
 ];
 
 this.addEventListener('install', function(event) {
+
+  // 注册缓存资源
   event.waitUntil(
     caches.open(CACHE_VERSION).then(function(cache) {
       return cache.addAll(CACHE_FILE_LIST);
@@ -19,6 +21,7 @@ this.addEventListener('install', function(event) {
   );
 });
 
+// 只监听 scope 目录下的资源
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
@@ -28,6 +31,7 @@ this.addEventListener('fetch', function(event) {
   );
 });
 
+// 激活状态
 this.addEventListener('activate', function(event) {
   var cacheWhitelist = [CACHE_VERSION];
   event.waitUntil(
@@ -39,24 +43,26 @@ this.addEventListener('activate', function(event) {
       }));
     })
   );
-
 });
 
-function requestBackend(event) {  // 请求备份操作
-  var url = event.request.clone();
-  return fetch(url)
+// 请求备份操作
+function requestBackend(event) {  
+  var req = event.request.clone();
+  return fetch(req)
     .then(function(res) { // 请求线上资源
       //if not a valid response send the error
       if (!res || res.status !== 200 || res.type !== 'basic') {
         return res;
       }
+      // 缓存从线上获取的资源
       var response = res.clone();
-      caches.open(CACHE_VERSION).then(function(cache) { // 缓存从线上获取的资源
+      caches.open(CACHE_VERSION).then(function(cache) { 
         cache.put(event.request, response);
       });
       return res;
     })
     .catch(function () {
+      // 找不到资源 用 404图片 代替
       return caches.match('/vendor/404.jpg');
     });
 }
